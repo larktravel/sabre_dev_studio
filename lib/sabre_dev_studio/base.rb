c1c3a39d3d27371a23514a132803904ef205732b
Adam Singer <adam@artforge.com>
2014-08-04 11:05:06 -0400

    logging & post support

--- lib/sabre_dev_studio/base.rb
+++ lib/sabre_dev_studio/base.rb
@@ -2,11 +2,54 @@ require 'httparty'
 require 'base64'
 
 module SabreDevStudio
+
+  class LogTimer
+
+    def initialize(name, params = {})
+      options = {
+        enabled: true,
+        display_start: false
+        }.merge(params)
+
+        @enabled = options[:enabled]
+        @display_start = options[:display_start]
+        @name = name
+      end
+
+      def start
+        return self unless @enabled
+        @start_time = Time.now
+        Rails.logger.info("START - #{@name}") if @display_start
+        return self
+      end
+
+      def check(message)
+        return unless @enabled
+        check_time = Time.now
+        duration = check_time - @start_time
+        Rails.logger.info("CHECK (#{message}) - #{@name} [#{(duration * 1000).to_i}ms]")
+      end
+
+      def stop
+        return unless @enabled
+        @stop_time = Time.now
+        duration = @stop_time - @start_time
+        if @display_start
+          Rails.logger.info("END --- #{@name} [#{(duration * 1000).to_i}ms]")
+        else
+          Rails.logger.info("[#{(duration * 1000).to_i}ms] --- #{@name}")
+        end
+
+      end
+
+    end
+
   class << self
     attr_accessor :configuration
   end
 
   def self.configure
+    Rails.logger.info("CONFIGURE")
     self.configuration ||= Configuration.new
     yield(configuration)
   end
@@ -28,6 +71,8 @@ module SabreDevStudio
     end
 
     def self.get_access_token
+      timer = LogTimer.new("** Base.get_access_token", {enabled: true}).start
+
       uri           = SabreDevStudio.configuration.uri
       client_id     = Base64.strict_encode64(SabreDevStudio.configuration.client_id)
       client_secret = Base64.strict_encode64(SabreDevStudio.configuration.client_secret)
@@ -38,10 +83,13 @@ module SabreDevStudio
                             :ssl_version => :TLSv1,
                             :verbose     => true,
                             :headers     => headers)
+      timer.stop
       @@token       = req['access_token']
     end
 
     def self.get(path, options = {})
+      timer = LogTimer.new("** Base.get #{path}", {enabled: true}).start
+
       attempt = 0
       begin
         attempt += 1
@@ -57,6 +105,40 @@ module SabreDevStudio
           :headers     => headers
         )
         verify_response(data)
+
+        timer.stop
+
+        return data
+      rescue SabreDevStudio::Unauthorized
+        if attempt == 1
+          get_access_token
+          retry
+        end
+      end
+    end
+
+    def self.send_post(path, options = {})
+      timer = LogTimer.new("** Base.get #{path}", {enabled: true}).start
+
+      attempt = 0
+      begin
+        attempt += 1
+        get_access_token if @@token.nil?
+        headers = {
+          'Authorization'   => "Bearer #{@@token}",
+          'Accept-Encoding' => 'gzip',
+          'Content-Type' => 'application/json'
+        }
+        data = post(
+          SabreDevStudio.configuration.uri + path,
+          :query       => options[:query],
+          :ssl_version => :TLSv1,
+          :headers     => headers
+        )
+        verify_response(data)
+
+        timer.stop
+
         return data
       rescue SabreDevStudio::Unauthorized
         if attempt == 1
